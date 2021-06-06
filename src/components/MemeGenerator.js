b0bb87a8a1ea6f664c4621c05355811126d9a5bf
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-06-06 00:05:09 -0700

    meme generator finito

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -13,6 +13,9 @@ class MemeGenerator extends React.Component {
             allMemeImgs : []
         }
         this.handleChange = this.handleChange.bind(this)
+
+        //binding is necessary for "this" to work in the callback
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     componentDidMount() {
@@ -36,10 +39,30 @@ class MemeGenerator extends React.Component {
         )
     }
 
+    //class method that is invoked onSubmit of form element
+    handleSubmit(event) {
+        //since form is submitted, event.preventDefault is called so that the page is not refreshed:
+        event.preventDefault()
+
+        //picking a random integer between 0 and last index of allMemeImgs array
+        const randomInt = Math.floor(Math.random() * this.state.allMemeImgs.length)
+        //grabbing the url of a meme at a random index of the allMemeImgs array
+        const randomMemeUrl = this.state.allMemeImgs[randomInt].url
+
+        //setting the randomImg property to the randomly picked meme's url,
+        //which will be plugged into the <img /> as a src="{randomImg}":
+        this.setState(
+            {
+            randomImg : randomMemeUrl
+            }
+        )
+    }
+
     render() {
         return (
             <>
-                <form className="meme-form">
+                {/* onSubmit on form element was used vs. onClick on button (better in terms of semantic HTML) */}
+                <form className="meme-form" onSubmit={this.handleSubmit}>
 
                     <input
                         type="text"
@@ -51,7 +74,7 @@ class MemeGenerator extends React.Component {
 
                    <input
                         type="text"
-                        name="topText"
+                        name="bottomText"
                         placeholder="Bottom Text"
                         value={this.state.bottomText}
                         onChange={this.handleChange}
@@ -59,6 +82,12 @@ class MemeGenerator extends React.Component {
 
                     <button>Generate</button>
                 </form>
+
+                <div className="meme">
+                    <img src={this.state.randomImg} alt="randomly generated meme"/>
+                    <h2 className="top">{this.state.topText}</h2>
+                    <h2 className="bottom">{this.state.bottomText}</h2>
+                </div>
             </>
         )
     }
