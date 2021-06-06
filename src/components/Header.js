0d252b6599372ba4ecb15a9a2ebaae51b58686ed
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-06-05 17:30:53 -0700

    initialised necessary components && cleaned up App/index.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+//functional component since no need to handle state
+const Header = () => {
+    return (
+        <>
+            <h1>MEME GENERATOR</h1>
+        </>
+    )
+}
+
+export default Header
---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+//class-based component since it will need to pull API data
+//and also storing that data in state:
+
+class MemeGenerator extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return (
+
+            )
+    }
+}
+
+export default MemeGenerator
